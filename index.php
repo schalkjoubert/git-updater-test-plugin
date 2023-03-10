498d536d332a6bced0695e1cf2b46dcc5084a218
Schalk Joubert <schalk@thissideup.media>
2023-03-10 08:37:57 +0200

    Update to own repo

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 /*
 Plugin Name: Example plugin
 Description: We will demonstrate how to make it git-updater compatible
-Version:     1.0.2
+Version:     1.0.1
 Author:      Andy & Maciek
 License:     GPL2 etc
-GitHub Plugin URI: palmiak/git-updater-test-plugin
+GitHub Plugin URI: schalkjoubert/git-updater-test-plugin
 Primary Branch: main
 Release Asset: true
 */
 
 add_action( 'admin_notices', 'test_print_notice', 0, 0 );
 function set_text() {
-	return 'test_2';
+	return 'test_1';
 }
 
 function test_print_notice() {
